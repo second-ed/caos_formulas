d5d3f12a084da78e4a826d915231686535ad60d6
gazorpazorpius <edcussmusic@gmail.com>
2024-03-23 13:34:22 +0000

    logic_gate_usage

--- src/logic_gates.py
+++ src/logic_gates.py
@@ -1,4 +1,26 @@
-from typing import Tuple
+from typing import List, Tuple
+import pandas as pd
+from itertools import product
+import inspect
+
+
+def generate_inputs(n):
+    return list(product([0, 1], repeat=n))
+
+
+def get_logic_outputs(inputs: List[Tuple], func):
+    outputs = []
+    for input in inputs:
+        outputs.append((*input, *func(*input)))
+    return outputs
+
+
+def get_results_df(func, output_cols: List[str]):
+    params = inspect.signature(func).parameters.keys()
+    n: int = len(params)
+    input_cols = list(params)
+    return pd.DataFrame(get_logic_outputs(generate_inputs(n), func), columns=input_cols + output_cols)
+
 
 
 def buffer(x) -> int:
