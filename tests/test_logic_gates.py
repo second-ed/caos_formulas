26dc2e7e809b983a93560811f388cbfdcf1df764
gazorpazorpius <edcussmusic@gmail.com>
2024-03-24 23:51:54 +0000

    type hinting to tests

--- tests/test_logic_gates.py
+++ tests/test_logic_gates.py
@@ -1,3 +1,4 @@
+from typing import Callable, List, Tuple
 import pytest
 from src.logic_gates import (
     and_gate,
@@ -14,38 +15,38 @@ from src.logic_gates import (
 )
 
 
-def and_into_inverse(x, y):
+def and_into_inverse(x, y) -> Tuple[int]:
     z = inverter_gate(and_gate(x, y))
     return (z,)
 
 
 @pytest.mark.parametrize("x, y, expected", [(1, 1, 1), (1, 0, 0), (0, 1, 0), (0, 0, 0)])
-def test_and_gate(x, y, expected):
+def test_and_gate(x: int, y: int, expected: int) -> None:
     assert and_gate(x, y) == expected
 
 
 @pytest.mark.parametrize("x, y, expected", [(1, 1, 1), (1, 0, 1), (0, 1, 1), (0, 0, 0)])
-def test_or_gate(x, y, expected):
+def test_or_gate(x: int, y: int, expected: int) -> None:
     assert or_gate(x, y) == expected
 
 
 @pytest.mark.parametrize("x, y, expected", [(1, 1, 0), (1, 0, 1), (0, 1, 1), (0, 0, 1)])
-def test_nand_gate(x, y, expected):
+def test_nand_gate(x: int, y: int, expected: int) -> None:
     assert nand_gate(x, y) == expected
 
 
 @pytest.mark.parametrize("x, y, expected", [(1, 1, 0), (1, 0, 0), (0, 1, 0), (0, 0, 1)])
-def test_nor_gate(x, y, expected):
+def test_nor_gate(x: int, y: int, expected: int) -> None:
     assert nor_gate(x, y) == expected
 
 
 @pytest.mark.parametrize("x, y, expected", [(1, 1, 0), (1, 0, 1), (0, 1, 1), (0, 0, 0)])
-def test_xor_gate(x, y, expected):
+def test_xor_gate(x: int, y: int, expected: int) -> None:
     assert xor_gate(x, y) == expected
 
 
 @pytest.mark.parametrize("x, y, expected", [(1, 1, 1), (1, 0, 0), (0, 1, 0), (0, 0, 1)])
-def test_xnor_gate(x, y, expected):
+def test_xnor_gate(x: int, y: int, expected: int) -> None:
     assert xnor_gate(x, y) == expected
 
 
@@ -58,7 +59,7 @@ def test_xnor_gate(x, y, expected):
         (1, 1, 0, 1),
     ],
 )
-def test_half_adder(x, y, expected_sum, expected_carry):
+def test_half_adder(x: int, y: int, expected_sum: int, expected_carry: int) -> None:
     assert half_adder(x, y) == (expected_sum, expected_carry)
 
 
@@ -75,7 +76,7 @@ def test_half_adder(x, y, expected_sum, expected_carry):
         (1, 1, 1, 1, 1),
     ],
 )
-def test_full_adder(x, y, c_in, expected_sum, expected_carry):
+def test_full_adder(x: int, y: int, c_in: int, expected_sum: int, expected_carry: int) -> None:
     assert full_adder(x, y, c_in) == (expected_sum, expected_carry)
 
 
@@ -119,7 +120,7 @@ def test_full_adder(x, y, c_in, expected_sum, expected_carry):
         ),
     ],
 )
-def test_generate_inputs(n, expected_output):
+def test_generate_inputs(n: int, expected_output: List[Tuple]) -> None:
     assert generate_inputs(n) == expected_output
 
 
@@ -133,5 +134,5 @@ def test_generate_inputs(n, expected_output):
         )
     ],
 )
-def test_get_logic_outputs(inputs, func, expected_output):
+def test_get_logic_outputs(inputs: List[Tuple], func: Callable, expected_output: List[Tuple]) -> None:
     assert get_logic_outputs(inputs, func) == expected_output
