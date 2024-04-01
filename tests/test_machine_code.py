4c4de901c228f549b8c85e3a19307ccb1bc1ce04
gazorpazorpius <edcussmusic@gmail.com>
2024-04-01 15:55:58 +0100

    test program

--- tests/test_machine_code.py
+++ tests/test_machine_code.py
@@ -1,6 +1,21 @@
 import pytest
 
-from src.machine_code_programs import ADD, DIV, EQ, GT, LDI, LT, MUL, NZ, SUB, ZERO
+from src.machine_code_programs import (
+    ADD,
+    DEC,
+    DIV,
+    EQ,
+    GT,
+    HALT,
+    INC,
+    LDI,
+    LT,
+    MUL,
+    NZ,
+    SUB,
+    ZERO,
+    run_program,
+)
 
 
 @pytest.mark.parametrize(
@@ -149,24 +164,32 @@ def test_LDI(memory, Rx, nn, expected_result) -> None:
 #     assert STORE(memory, Rx, Ry) == expected_result
 
 
-# @pytest.mark.parametrize(
-#     "memory, Rx, expected_result",
-#     [
-#         (memory, Rx, expected_result),
-#     ]
-# )
-# def test_INC(memory, Rx, expected_result) -> None:
-#     assert INC(memory, Rx) == expected_result
+@pytest.mark.parametrize(
+    "memory, Rx, expected_result",
+    [
+        ([0, 0], 0, [1, 0]),
+        ([0, 0], 1, [0, 1]),
+        ([1, 0], 0, [2, 0]),
+        ([1, 0], 1, [1, 1]),
+    ],
+)
+def test_INC(memory, Rx, expected_result) -> None:
+    INC(memory, Rx)
+    assert memory == expected_result
 
 
-# @pytest.mark.parametrize(
-#     "memory, Rx, expected_result",
-#     [
-#         (memory, Rx, expected_result),
-#     ]
-# )
-# def test_DEC(memory, Rx, expected_result) -> None:
-#     assert DEC(memory, Rx) == expected_result
+@pytest.mark.parametrize(
+    "memory, Rx, expected_result",
+    [
+        ([0, 0], 0, [-1, 0]),
+        ([0, 0], 1, [0, -1]),
+        ([1, 0], 0, [0, 0]),
+        ([1, 0], 1, [1, -1]),
+    ],
+)
+def test_DEC(memory, Rx, expected_result) -> None:
+    DEC(memory, Rx)
+    assert memory == expected_result
 
 
 # @pytest.mark.parametrize(
@@ -219,11 +242,62 @@ def test_LDI(memory, Rx, nn, expected_result) -> None:
 #     assert HALT() == expected_result
 
 
-# @pytest.mark.parametrize(
-#     "program, memory, expected_result",
-#     [
-#         (program, memory, expected_result),
-#     ]
-# )
-# def test_run_program(program, memory, expected_result) -> None:
-#     assert run_program(program, memory) == expected_result
+@pytest.mark.parametrize(
+    "program, memory, expected_result",
+    [
+        (
+            [
+                (LDI, (1, 100)),
+                (LDI, (2, 25)),
+                (DIV, (1, 2, 3)),
+                (HALT, ()),
+            ],
+            [0 for _ in range(16)],
+            [0, 100, 25, 4.0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ),
+        (
+            [
+                (LDI, (1, 5)),
+                (LDI, (2, 7)),
+                (LDI, (3, 9)),
+                (ADD, (1, 2, 1)),
+                (ADD, (1, 3, 1)),
+                (LDI, (4, 3)),
+                (DIV, (1, 4, 1)),
+                (HALT, ()),
+            ],
+            [0 for _ in range(16)],
+            [0, 7.0, 7, 9, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ),
+        (
+            [
+                (LDI, (0, 9)),
+                (LDI, (1, 3)),
+                (LDI, (2, 0)),
+                (LDI, (3, 0)),
+                (ADD, (0, 1, 2)),
+                (MUL, (2, 0, 3)),
+                (HALT, ()),
+            ],
+            [0 for _ in range(16)],
+            [9, 3, 12, 108, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ),
+        (
+            [
+                (LDI, (1, 3)),
+                (LDI, (2, 5)),
+                (LDI, (3, 6)),
+                (LDI, (4, 4)),
+                (LDI, (6, 0)),
+                (MUL, (1, 2, 1)),
+                (MUL, (3, 4, 3)),
+                (SUB, (3, 1, 6)),
+                (HALT, ()),
+            ],
+            [0 for _ in range(16)],
+            [0, 15, 5, 24, 4, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ),
+    ],
+)
+def test_run_program(program, memory, expected_result) -> None:
+    assert run_program(program, memory) == expected_result
