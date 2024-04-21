ce8cea6862b11ba6b754655113266478cf6b1aa6
gazorpazorpius <edcussmusic@gmail.com>
2024-04-21 13:13:00 +0100

    linted text_compression

--- src/text_compression.py
+++ src/text_compression.py
@@ -8,12 +8,14 @@ def count_compressed_bits(text, default_size, encoding_map) -> int:
     return total_bits_compressed
 
 
-def compress_text(text, encoding_map, default_bits = 8) -> tuple:
+def compress_text(text, encoding_map, default_bits=8) -> tuple:
     text_len = len(text)
     char_counts = {k: text.count(k) for k in set(text)}
 
-    all_chars_by_freq = sorted(char_counts, key= lambda x: char_counts[x], reverse=True) 
-    encoded_chars_by_freq = [c for c in all_chars_by_freq if c in encoding_map] + [c for c in encoding_map if c not in all_chars_by_freq]
+    all_chars_by_freq = sorted(char_counts, key=lambda x: char_counts[x], reverse=True)
+    encoded_chars_by_freq = [c for c in all_chars_by_freq if c in encoding_map] + [
+        c for c in encoding_map if c not in all_chars_by_freq
+    ]
 
     lowest_bits = sorted(encoding_map.values())
     optimised_encoding_map = dict(zip(encoded_chars_by_freq, lowest_bits))
@@ -21,21 +23,25 @@ def compress_text(text, encoding_map, default_bits = 8) -> tuple:
 
     no_compression = count_compressed_bits(text, default_bits, {})
     actual_compression = count_compressed_bits(text, default_bits, encoding_map)
-    optimised_compression = count_compressed_bits(text, default_bits, optimised_encoding_map)
-    true_optimised_compression = count_compressed_bits(text, default_bits, true_optimised_encoding_map)
+    optimised_compression = count_compressed_bits(
+        text, default_bits, optimised_encoding_map
+    )
+    true_optimised_compression = count_compressed_bits(
+        text, default_bits, true_optimised_encoding_map
+    )
 
     actual_compression_ratio = no_compression / actual_compression
     optimised_compression_ratio = no_compression / optimised_compression
     true_optimised_compression_ratio = no_compression / true_optimised_compression
-    
+
     for k, v in vars().items():
         print(f"{k} = {v}")
 
     return (
-        text, 
+        text,
         encoding_map,
         default_bits,
-        text_len, 
+        text_len,
         char_counts,
         all_chars_by_freq,
         lowest_bits,
@@ -45,5 +51,5 @@ def compress_text(text, encoding_map, default_bits = 8) -> tuple:
         optimised_compression,
         actual_compression_ratio,
         optimised_compression_ratio,
-        true_optimised_compression_ratio
+        true_optimised_compression_ratio,
     )
